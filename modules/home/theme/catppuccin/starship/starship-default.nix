d61591b273844502b169d54781db4720c771522b
Bautista <bauti@example.com>
2026-09-24 10:49:39 -0300

    add(starhip-catppuccin): hostname ssh

--- modules/home/theme/catppuccin/starship/starship-default.nix
+++ modules/home/theme/catppuccin/starship/starship-default.nix
@@ -2,7 +2,7 @@ let
   catppuccin = import ../colors.nix;
 in 
 {
-  format = "$username[](bg:${catppuccin.palette.surface0.hex} fg:${catppuccin.palette.mauve.hex})$directory[](fg:${catppuccin.palette.surface0.hex} bg:${catppuccin.palette.blue.hex})$git_branch$git_status[](fg:${catppuccin.palette.blue.hex} bg:${catppuccin.palette.yellow.hex})$nodejs$bun$rust$golang$php[](fg:${catppuccin.palette.yellow.hex} bg:${catppuccin.palette.green.hex})$time[ ](fg:${catppuccin.palette.green.hex})$character";
+  format = "$hostname$username[](bg:${catppuccin.palette.surface0.hex} fg:${catppuccin.palette.mauve.hex})$directory[](fg:${catppuccin.palette.surface0.hex} bg:${catppuccin.palette.blue.hex})$git_branch$git_status[](fg:${catppuccin.palette.blue.hex} bg:${catppuccin.palette.yellow.hex})$nodejs$bun$rust$golang$php[](fg:${catppuccin.palette.yellow.hex} bg:${catppuccin.palette.green.hex})$time[ ](fg:${catppuccin.palette.green.hex})$character";
 
   directory = {
     style = "fg:${catppuccin.palette.text.hex} bg:${catppuccin.palette.surface0.hex}";
@@ -19,6 +19,11 @@ in
     disabled = false;
     style_user = "bg:${catppuccin.palette.mauve.hex} fg:${catppuccin.palette.base.hex}";
     format = "[     $user  ]($style)";
+  };
+  hostname = {
+    style = "bg:${catppuccin.palette.mauve.hex} fg:${catppuccin.palette.base.hex}";
+    ssh_symbol = "";
+    format = "[ $hostname]($style)";
   };
 
   git_branch = {
