3895a182d114b6756deb3dcaba05261531550709
Bautista <bauti@example.com>
2026-09-24 12:18:48 -0300

    feat(syncthing): syncthing syncs lab and pc

--- modules/nixos/services/syncthing/default.nix
+++ modules/nixos/services/syncthing/default.nix
@@ -9,6 +9,8 @@ let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.bautinix.services.syncthing;
+  username = config.bautinix.user.name;
+
 in
 {
   options.bautinix.services.syncthing = {
@@ -21,9 +23,29 @@ in
 
       enable = true;
 
-      user = "${config.bautinix.user.name}";
-      dataDir = "/home/${config.bautinix.user.name}/.local/share/syncthing";
-      configDir = "/home/${config.bautinix.user.name}/.config/syncthing";
+      user = "${username}";
+
+      devices = {
+        lab-nixos = {
+          adresses = [ "tcp://100.97.207.53:51820" ];
+          id = "7S6HATB-QZHADNN-WNRL65U-PASUOO3-DA6OJ3E-RQ4OI7W-DN3OEDP-BOKSQA4";
+        };
+        pc-nixos = {
+          adresses = [ "tcp://100.113.124.111:51820" ];
+          id = "D4JLFS2-VW2FKLH-MFTYFQ2-VE5TYRH-T5XOGCP-W6WECZN-MU7WBHL-QI2QTQA";
+        };
+      };
+
+      folders = {
+        "/home/${username}/syncthing" = {
+        id = "root";
+        devices = [ "pc-nixos" "lab-nixos"];
+ 
+        };
+      };
+
+      dataDir = "/home/${username}/syncthing";
+      configDir = "/home/${username}/.config/syncthing";
 
       openDefaultPorts = true;
       guiAddress = "127.0.0.1:8384" ;
